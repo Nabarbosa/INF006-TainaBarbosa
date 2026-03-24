1e5eb5f32f394284125d2ac6127e79d5e99b926d
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2026-03-24 00:36:00 +0000

    atualizado

--- insertion_sort.c
+++ insertion_sort.c
@@ -3,10 +3,18 @@
 #include <time.h>
 #include <stdbool.h>
 
-#define TAM 6
+#define TAM 1000000
 
 void insertion_sort(int *A, int size){
-    
+    for(int j = 1; j < size; j++){
+        int key = A[j];
+        int i = j - 1;
+        while(i >= 0 && A[i] > key){
+            A[i+1] = A[i];
+            i = i - 1;
+        }
+        A[i + 1] = key;
+    }
 }
 
 void print_array(int * valores, int tam){
@@ -26,7 +34,16 @@ int main(){
     for(int i = 0; i < TAM; i++){
         valores[i]= rand()%100; // Rand - gerar numeros aleatórios
     }
+
     print_array(valores, TAM);
-    
+    time_t inicio = clock();
+    insertion_sort(valores, TAM);
+    time_t final = clock();
+
+    float diferenca = (final - inicio) /CLOCKS_PER_SEC;
+
+    print_array(valores, TAM);
+    printf("\nPara ordenar o array foram gastos %.6f segundos\n", diferenca);
+
     return 0;
 }
